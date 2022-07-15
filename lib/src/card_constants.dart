9482d88f45dc2145d519f5b98f673640a2d9597f
sebastian.montero <sebastian.montero@globant.com>
2022-07-15 17:24:10 -0300

    hearthstone card creation
    added new asset to yaml
    
    created new file with all the constants, imported the file, relative import,  modified according good practices, trailing commas and descriptive class name.
    
    Added 2 new consts, imported card_constants to main, created folders for latter use.

---
+++ lib/src/card_constants.dart
@@ -0,0 +1,25 @@
+const String materialAppTitle = 'Hearthstone cards';
+const String appBarTitle = 'Is this your card?';
+const String assetPathBackgroundImage = 'assets/images/hsbackground.png';
+const String assetPathCardImage = 'assets/images/leper.png';
+const String cardTextInfo = 'Name: Leper Gnome\n'
+    'Type: Minion\n'
+    'Faction: Neutral\n'
+    'Rarity: Common\n'
+    'Cost: 1\n'
+    'Attack: 2\n'
+    'Health: 1\n'
+    'Ability:Deathrattle: Deal 2 damage to the enemy hero.\n';
+const String counterText = 'Likes';
+const int counterInitialValue = 0;
+const double appBarTitleFontSize = 35.0;
+const double backgroundImageContainerHeight = 1100.0;
+const double cardTextPaddingLeftTopRight = 50.0;
+const double cardTextPaddingBottom = 0.1;
+const double cardTextInfoFontSize = 25.0;
+const double cardImageBoxSizeWidth = 600.0;
+const double cardImageBoxSizeHeight = 600.0;
+const double elevatedButtonElevationValue = 24.3;
+const double elevatedButtonShapeBorderRadiusValue = 40.4;
+const double elevatedButtonSideBorderWidthRadiusValue = 5;
+const double counterTextFontSize = 50.0;
